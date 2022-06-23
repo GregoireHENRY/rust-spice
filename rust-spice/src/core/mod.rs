153d789dfb40dd7d86d3146c3d4915ea089b74f3
Caleb Leinz <cleinz+github@pm.me>
2022-06-23 16:15:34 -0700

    Changed visibility of new functions

--- rust-spice/src/core/mod.rs
+++ rust-spice/src/core/mod.rs
@@ -165,9 +165,10 @@ pub mod result;
 
 pub use self::neat::{bodc2n, dskp02, dskv02, kdata, timout};
 pub use self::raw::{
-    bodfnd, bodn2c, dascls, dasopr, dlabfs, dskgd, dskn02, dskobj, dskx02, dskz02, furnsh, georec,
-    illumf, kclear, ktotal, latrec, mxv, pxform, pxfrm2, radrec, recazl, recrad, spkcls, spkezr,
-    spkopn, spkpos, spkw09, str2et, unload, vcrss, vdot, vsep, xpose, DLADSC, DSKDSC,
+    bodfnd, bodn2c, dascls, dasopr, dlabfs, dskgd, dskn02, dskobj, dskx02, dskz02, erract, failed,
+    furnsh, georec, getmsg, illumf, kclear, ktotal, latrec, mxv, pxform, pxfrm2, radrec, recazl,
+    recrad, spkcls, spkezr, spkopn, spkpos, spkw09, str2et, unload, vcrss, vdot, vsep, xpose,
+    DLADSC, DSKDSC,
 };
 pub use self::result::{furnsh_safe, SpiceError};
 
