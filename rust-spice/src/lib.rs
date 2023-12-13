b6a6bc13cefb61dd47adf931bbf8f39d6ca9236c
Grégoire Henry <gregoire.henry@oma.be>
2023-12-13 14:19:13 +0100

    cspice-sys from git as feature for build auto-DL

--- rust-spice/src/lib.rs
+++ rust-spice/src/lib.rs
@@ -9,6 +9,9 @@
 #[cfg(feature = "default")]
 extern crate cspice_sys;
 
+#[cfg(feature = "git")]
+extern crate cspice_sys_git as cspice_sys;
+
 #[cfg(feature = "noclang")]
 extern crate cspice_sys_no_clang as cspice_sys;
 
